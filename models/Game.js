a1a22083b945adc903f3a3fd625dc9e2896a65b1
Thomas Pettinger <pettinger.thomas@gmail.com>
2016-10-23 04:32:20 +0200

    syntax fix

--- models/Game.js
+++ models/Game.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
 var GameSchema = new mongoose.Schema({
-    maxScore: number,
-    score1: {type: number, default: 0},
-    score2: {type: number, default: 0}
+    maxScore: Number,
+    score1: {type: Number, default: 0},
+    score2: {type: Number, default: 0}
 });
 
 mongoose.model("Game", GameSchema);
