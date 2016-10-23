b60b234631a157479f520d5b66d0602d9884522f
Krillian111 <krillian@gmx.de>
2016-10-23 20:10:59 +0200

    updated example to have a working post and get method

--- routes/test.js
+++ routes/test.js
@@ -18,4 +18,42 @@ router.get('/', function (req, res, next) {
     res.json(myPlayer);
 });
 
+router.post('/player/add', function (req, res, next) {
+    console.log('start');
+    var name = req.query.playername;
+    console.log('name:' + name);
+    if (typeof name === 'string') {
+        mongoose.model('Player').create({
+            name: name,
+            elo: '1500'
+        }, function (err, player) {
+            if (err) {
+                res.send('There was a problem adding the information to the database');
+            } else {
+                res.json({id: player._id})
+            }
+        });
+    } else {
+        res.status(400);
+    }
+});
+
+router.get('/player/:id', function (req, res, next) {
+    mongoose.model('Player').findById(req.params.id, function (err, player) {
+        if (err) {
+            console.log('GET Error: ' + err);
+            res.status(500);
+            res.send('GET request failed');
+        } else {
+            if (player === null) {
+                console.log('GET Error: Player with ID ' + req.params.id + ' does not exist');
+                res.status(400);
+            } else {
+                console.log('GET Player with ID: ' + player);
+                res.json(player);
+            }
+        }
+    })
+});
+
 module.exports = router;
