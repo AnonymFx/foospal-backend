a8b15d82dd26fa31b598700ef9d72703e9b0ddc3
Thomas Pettinger <pettinger.thomas@gmail.com>
2016-10-23 04:11:40 +0200

    update database name

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/dbname') //TODO: change db name
+mongoose.connect('mongodb://localhost/foospal') 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
