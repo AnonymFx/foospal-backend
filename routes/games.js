7f437e43b16d5fbb144e177774751599a54832df
Thomas Pettinger <pettinger.thomas@gmail.com>
2016-10-29 11:35:09 +0200

    added todos

--- routes/games.js
+++ routes/games.js
@@ -13,7 +13,10 @@ router.get('/', function(req, res, next) {
 router.post('/', function (req, res, next){
     var game = new Game(req.body)
     console.log('post game: ' + game.maxScore + ', ' + game.score1 + ', ' + game.score2);
-    //TODO: error handling
+    /*TODO: error handling
+     * Find tournament and check if the scores fit
+     * Also transmission of the match the games belongs to is missing
+     */
     game.save().then(function(game) {
         console.log('Saving new game successfull');
         res.json(game)
