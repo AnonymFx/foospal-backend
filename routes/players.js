ecc3122c452af5cd9c5ccf730530e1e9fd451046
Krillian111 <krillian@gmx.de>
2016-11-21 00:26:19 +0100

    refactored validation for post(:/playerid)

--- routes/players.js
+++ routes/players.js
@@ -6,49 +6,68 @@ var Player = mongoose.model('Player');
 
 router.post('/add', function (req, res, next) {
     var player = new Player(req.body);
-    /* TODO: error handling */
+    /* TODO: error handling and validation (see post playerid as template)*/
     player.save().then(function (player) {
-        console.log('POST: Saving player successful: ID = ' + player._id + ', Name = ' + player.name + ', ELO = ' + player.elo);
+        console.log('POST successful: Saving player (ID = ' + player._id + ', Name = ' + player.name + ', ELO = ' + player.elo + ')');
         res.json(player);
     }, function (err) {
-        console.log('POST: Could not save player ' + player.name + '; error: ' + err);
+        console.log('POST failed: Could not save player ' + player.name + '; internal error: ' + err);
         res.status(500);
         res.send();
     });
 });
 
 router.post('/:playerid', function (req, res, next) {
-    var playerid = req.params.playerid;
+    var playerId = req.params.playerid;
     var name = req.body.name;
     var elo = req.body.elo;
-    if (!isObjectIdValid(playerid)) {
-        respondWithHttpError(res, 400, 'GET failed: malformed id');
-    } else {
-        // TODO: error handling
-        Player.findById(playerid).exec()
-            .then(function (player) {
-                // update player
-                // TODO: refactor if better "javascript way" exists
-                if (name !== undefined) {
-                    player.name = name;
-                }
-                if (elo !== undefined || !(typeof elo === 'number')) {
-                    player.elo = elo;
-                }
-                player.save();
-                console.log('updated player: name: ' + player.name + ', elo:' + elo);
-                res.json(player);
-            })
-            .catch(function (err) {
-                // error handling
-                console.log('ERROR during post via playerid: ' + err);
-                res.status(500).send();
-            });
+    var validationResult = validatePostPlayerRequest(name, elo, playerId);
+    if (validationResult.valid === false) {
+        respondWithHttpError(res, 400, validationResult.errormsg);
     }
+    Player.findById(playerId).exec()
+        .then(function (player) {
+            if (player == null) {
+                respondWithHttpError(res, 400, "POST failed: no player with requested id available");
+            } else {
+                player.name = name;
+                player.elo = elo;
+                player.save();
+                console.log('POST successful: updated player successful (Name = ' + name + ', ELO = ' + elo + ')');
+                res.json(player);
+            }
+        })
+        .catch(function (err) {
+            // error handling
+            console.log('POST failed: Could not update player with id ' + playerId + '; internal error:' + err);
+            res.status(500).send();
+        });
 });
+
+function validatePostPlayerRequest(name, elo, playerid) {
+    var validationErrorMsg = '';
+    if (typeof name !== 'string') {
+        validationErrorMsg += ' "name" is not a string;';
+    }
+    if (typeof elo !== 'number') {
+        validationErrorMsg += ' "elo" is not a number;';
+    }
+    if (!isObjectIdValid(playerid)) {
+        validationErrorMsg += ' playerid is not an ObjectId';
+    }
+    if (validationErrorMsg !== '') {
+        return {
+            valid: false,
+            errormsg: 'POST failed: Validation error - malformed request:' + validationErrorMsg
+        };
+    } else {
+        return {valid: true};
+    }
+}
 
 router.get('/:playerid', function (req, res, next) {
     var playerid = req.params.playerid;
+    // TODO: refactor validation (see post playerid as template)
     if (!isObjectIdValid(playerid)) {
         respondWithHttpError(res, 400, 'GET failed: malformed id');
     } else {
@@ -62,13 +81,14 @@ router.get('/:playerid', function (req, res, next) {
                 }
             })
             .catch(function (err) {
-                respondWithHttpError(res, 500, 'GET failed: internal error', err);
+                respondWithHttpError(res, 500, 'GET failed: Could not find player with id ' + playerid + ';internal error', err);
             });
     }
 });
 
 router.get('/page/:pageindex', function (req, res, next) {
     var pageindex = req.params.pageindex;
+    // TODO: refactor validation (see post playerid as template)
     if (typeof pageindex !== 'number') {
         respondWithHttpError(res, 404, "GET failed: pageindex not a number");
     } else {
@@ -85,15 +105,16 @@ router.get('/page/:pageindex', function (req, res, next) {
                 }
             })
             .catch(function (err) {
-                respondWithHttpError(res, 500, 'GET failed: internal error', err);
+                respondWithHttpError(res, 500, 'GET failed: Could not retrieve players from page ' + pageindex + ';internal error', err);
             });
     }
 });
 
 router.delete('/:playerid', function (req, res, next) {
+    // TODO: refactor validation (see post playerid as template)
     var playerid = req.params.playerid;
     if (!isObjectIdValid(playerid)) {
-        respondWithHttpError(res, 400, 'GET failed: malformed id');
+        respondWithHttpError(res, 400, 'DELETE failed: malformed id');
     } else {
         Player.findByIdAndRemove(playerid).exec()
             .then(function (player) {
@@ -120,7 +141,7 @@ function respondWithHttpError(res, errorcode, msg, err) {
         completeMsg += err;
     }
     console.log(completeMsg);
-    res.status(errorcode).send();
+    res.status(errorcode).send(msg);
 }
 
 module.exports = router;
