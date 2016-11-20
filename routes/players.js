fcff26d6a133e28a901849acbb7b6c8a8a205463
Krillian111 <krillian@gmx.de>
2016-11-20 22:35:56 +0100

    added check for valid objectId refactored error responses into separate method

--- routes/players.js
+++ routes/players.js
@@ -21,85 +21,106 @@ router.post('/:playerid', function (req, res, next) {
     var playerid = req.params.playerid;
     var name = req.body.name;
     var elo = req.body.elo;
-    console.log(elo);
-    // TODO: error handling
-    Player.findById(playerid).exec()
-        .then(function (player) {
-            // update player
-            // TODO: refactor if better "javascript way" exists
-            if (name !== undefined) {
-                player.name = name;
-            }
-            if (elo !== undefined || !(typeof elo === "number")) {
-                player.elo = elo;
-            }
-            player.save();
-            console.log('updated player: name: ' + player.name + ', elo:' + elo);
-            res.json(player);
-        })
-        .catch(function (err) {
-            // error handling
-            console.log('ERROR during post via playerid: ' + err);
-            res.status(500).send();
-        });
+    if (!isObjectIdValid(playerid)) {
+        respondWithHttpError(res, 400, 'GET failed: malformed id');
+    } else {
+        // TODO: error handling
+        Player.findById(playerid).exec()
+            .then(function (player) {
+                // update player
+                // TODO: refactor if better "javascript way" exists
+                if (name !== undefined) {
+                    player.name = name;
+                }
+                if (elo !== undefined || !(typeof elo === 'number')) {
+                    player.elo = elo;
+                }
+                player.save();
+                console.log('updated player: name: ' + player.name + ', elo:' + elo);
+                res.json(player);
+            })
+            .catch(function (err) {
+                // error handling
+                console.log('ERROR during post via playerid: ' + err);
+                res.status(500).send();
+            });
+    }
 });
 
 router.get('/:playerid', function (req, res, next) {
     var playerid = req.params.playerid;
-    // TODO: error handling, also see TODO in router.delete
-    Player.findById(playerid).exec()
-        .then(function (player) {
-            if (player == null) {
-                res.status(404).send();
-            } else {
-                console.log('GET successful: returning player with id ' + playerid);
-                res.json(player);
-            }
-        })
-        .catch(function (err) {
-            console.log('GET player by playerid failed: ' + err);
-            res.status(500).send();
-        });
+    if (!isObjectIdValid(playerid)) {
+        respondWithHttpError(res, 400, 'GET failed: malformed id');
+    } else {
+        Player.findById(playerid).exec()
+            .then(function (player) {
+                if (player == null) {
+                    respondWithHttpError(res, 404, "GET failed: no player with requested id available");
+                } else {
+                    console.log('GET successful: returning player with id ' + playerid);
+                    res.json(player);
+                }
+            })
+            .catch(function (err) {
+                respondWithHttpError(res, 500, 'GET failed: internal error', err);
+            });
+    }
 });
 
 router.get('/page/:pageindex', function (req, res, next) {
     var pageindex = req.params.pageindex;
-    // TODO: implement returning just a subset of players, currently all players are returned regardless of the pageindex
-    Player.find({}).exec()
-        .then(function (players) {
-            if (players.length === 0) {
-                console.log('GET succesful: no players -> returning empty list');
-                res.json({});
-            }
-            else {
-                console.log('GET successful: returning players with pageindex ' + pageindex);
-                res.json(players);
-            }
-        })
-        .catch(function (err) {
-            console.log('GET players by pageindex failed:' + err);
-            res.status(500).send();
-        });
+    if (typeof pageindex !== 'number') {
+        respondWithHttpError(res, 404, "GET failed: pageindex not a number");
+    } else {
+        // TODO: implement returning just a subset of players, currently all players are returned regardless of the pageindex
+        Player.find({}).exec()
+            .then(function (players) {
+                if (players.length === 0) {
+                    console.log('GET successful: no players -> returning empty list');
+                    res.json({});
+                }
+                else {
+                    console.log('GET successful: returning players with pageindex ' + pageindex);
+                    res.json(players);
+                }
+            })
+            .catch(function (err) {
+                respondWithHttpError(res, 500, 'GET failed: internal error', err);
+            });
+    }
 });
 
 router.delete('/:playerid', function (req, res, next) {
     var playerid = req.params.playerid;
-    // TODO: how to catch error when id not correct (different cases)?
-    // case 1: /IDWhichisTooShort -> err -> 500 (should be 404 but not all DB-errors are)
-    // case 2: /IDWithCorrectLengthButNotInDB -> 500 (should be 404, can be handled via if(player === null) but ugly)
-    Player.findByIdAndRemove(playerid).exec()
-        .then(function (player) {
-            if (player === null) {
-                res.status(404).send();
-            } else {
-                console.log('DELETE successful:  player with id ' + playerid + ' deleted!');
-                res.json(player);
-            }
-        })
-        .catch(function (err) {
-            console.log('DELETE player by playerid failed: ' + err);
-            res.status(500).send();
-        });
+    if (!isObjectIdValid(playerid)) {
+        respondWithHttpError(res, 400, 'GET failed: malformed id');
+    } else {
+        Player.findByIdAndRemove(playerid).exec()
+            .then(function (player) {
+                if (player === null) {
+                    respondWithHttpError(res, 404, "DELETE failed: no player with requested id available");
+                } else {
+                    console.log('DELETE successful:  player with id ' + playerid + ' deleted!');
+                    res.json(player);
+                }
+            })
+            .catch(function (err) {
+                respondWithHttpError(res, 500, 'DELETE failed: internal error', err);
+            });
+    }
 });
+
+function isObjectIdValid(id) {
+    return mongoose.Types.ObjectId.isValid(id);
+}
+
+function respondWithHttpError(res, errorcode, msg, err) {
+    var completeMsg = msg;
+    if (err !== undefined) {
+        completeMsg += err;
+    }
+    console.log(completeMsg);
+    res.status(errorcode).send();
+}
 
 module.exports = router;
