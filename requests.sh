75b438fc5d701361021846112d09238d9e5f6160
Krillian111 <krillian@gmx.de>
2016-12-03 18:00:05 +0100

    fixed typo in json data

--- requests.sh
+++ requests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 function put_game() {
     echo "posting game"
-    curl -X PUT -H "Content Type: application/json" -d "{\"maxScrore\": 2, \"score1\": 2, \"score2\": 1}" 127.0.0.1:3000/api/games
+    curl -X PUT -H "Content Type: application/json" -d "{\"maxScore\": 2, \"score1\": 2, \"score2\": 1}" 127.0.0.1:3000/api/games
 }
 
 function post_game() {
     if [[ -z ${1+x} ]]; then
         echo "please enter a game id"
     fi
-    curl -X POST -H "Content Type: application/json" -d "{\"maxScrore\": 2, \"score1\": 2, \"score2\": 1}" 127.0.0.1:3000/api/games
+    curl -X POST -H "Content Type: application/json" -d "{\"maxScore\": 2, \"score1\": 2, \"score2\": 1}" 127.0.0.1:3000/api/games
 }
 
 
